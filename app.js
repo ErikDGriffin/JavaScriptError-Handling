50ecfd881750cbc7a161db047c481ff53d48fb44
erik Griffin <griffin.erikd@gmail.com>
2023-07-11 18:47:11 -0400

    added exercise

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-// TODO: Enable strict mode
-
-// TODO: Fix the following parameter list
-function parseToJSON(data, data) {
-  // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
+" use scrict";
+function parseToJSON(data) {
+  try{
   return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
-
 let failData = "<h1>Hello World!</h1>"
 
 let passData = JSON.stringify({ success: true });
